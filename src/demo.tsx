bda743a3f4d755be143b514aeca20b47b0b5ed88
Jirei <younesso.email@gmail.com>
2023-11-20 03:51:48 +0100

    initial commit

---
+++ src/demo.tsx
@@ -0,0 +1,152 @@
+import { Cube, CubeFace } from './cube';
+import { useState } from 'react';
+import templeWaterImage from './assets/compressed_cube_images/temple-water.jpg';
+import templeImage from './assets/compressed_cube_images/temple.jpg';
+import temple2Image from './assets/compressed_cube_images/temple-2.jpg';
+import elephantsImage from './assets/compressed_cube_images/elephants.jpg';
+import cookingImage from './assets/compressed_cube_images/cooking.jpg';
+import statueImage from './assets/compressed_cube_images/statue.jpg';
+
+export default function Demo() {
+  const [currentFace, setCurrentFace] = useState<CubeFace>('front');
+  const handleRadioChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setCurrentFace(e.target.value as CubeFace);
+  return (
+    <div className='flex items-center justify-center bg-[#F5E1C6] h-screen'>
+      <div className='flex flex-col items-center gap-y-32 gap-x-32'>
+        <Cube
+          transitionDuration='1s'
+          sizeValue={30}
+          sizeUnit='vw'
+          currentFace={currentFace}
+          cubeFaces={{
+            front: (
+              <div className='w-full h-full'>
+                <img
+                  className='absolute object-cover w-full h-full'
+                  src={templeWaterImage}
+                  alt='Startup image'
+                />
+              </div>
+            ),
+            right: (
+              <div className='w-full h-full'>
+                <img
+                  className='absolute object-cover w-full h-full'
+                  src={templeImage}
+                  alt='Language school image'
+                />
+              </div>
+            ),
+            back: (
+              <div className='w-full h-full'>
+                <img
+                  className='absolute object-cover w-full h-full'
+                  src={temple2Image}
+                  alt='startup image'
+                />
+              </div>
+            ),
+            left: (
+              <div className='w-full h-full'>
+                <img
+                  className='absolute object-cover w-full h-full'
+                  src={elephantsImage}
+                  alt='startup image'
+                />
+              </div>
+            ),
+            top: (
+              <div className='w-full h-full'>
+                <img
+                  className='absolute object-cover w-full h-full'
+                  src={cookingImage}
+                  alt='startup image'
+                />
+              </div>
+            ),
+            bottom: (
+              <div className='w-full h-full'>
+                <img
+                  className='absolute object-cover w-full h-full'
+                  src={statueImage}
+                  alt='startup image'
+                />
+              </div>
+            ),
+          }}
+        />
+        <form className='text-black'>
+          <ul>
+            <li className='flex gap-x-3'>
+              <label htmlFor='front'>front</label>
+              <input
+                onChange={handleRadioChange}
+                id='front'
+                type='radio'
+                name='rotate-cube-side'
+                value='front'
+                checked={currentFace === 'front'}
+              />
+            </li>
+            <li className='flex gap-x-3'>
+              <label htmlFor='right'>right</label>
+              <input
+                onChange={handleRadioChange}
+                id='right'
+                type='radio'
+                name='rotate-cube-side'
+                value='right'
+                checked={currentFace === 'right'}
+              />
+            </li>
+            <li className='flex gap-x-3'>
+              <label htmlFor='back'>back</label>
+              <input
+                onChange={handleRadioChange}
+                id='back'
+                type='radio'
+                name='rotate-cube-side'
+                value='back'
+                checked={currentFace === 'back'}
+              />
+            </li>
+            <li className='flex gap-x-3'>
+              <label htmlFor='left'>left</label>
+              <input
+                onChange={handleRadioChange}
+                id='left'
+                type='radio'
+                name='rotate-cube-side'
+                value='left'
+                checked={currentFace === 'left'}
+              />
+            </li>
+            <li className='flex gap-x-3'>
+              <label htmlFor='top'>top</label>
+              <input
+                onChange={handleRadioChange}
+                id='top'
+                type='radio'
+                name='rotate-cube-side'
+                value='top'
+                checked={currentFace === 'top'}
+              />
+            </li>
+            <li className='flex gap-x-3'>
+              <label htmlFor='bottom'>bottom</label>
+              <input
+                onChange={handleRadioChange}
+                id='bottom'
+                type='radio'
+                name='rotate-cube-side'
+                value='bottom'
+                checked={currentFace === 'bottom'}
+              />
+            </li>
+          </ul>
+        </form>
+      </div>
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,92 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: ['class'],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
+    extend: {
+      transitionDuration: {
+        1500: '1500ms',
+      },
+      backgroundImage: {
+        hero: `
+        linear-gradient(rgba(0,105,181, 0.5),rgba(0,105,181, 0.5)),
+        url('/hero.jpg')`,
+        'cube-face': `linear-gradient(
+  45deg,
+  hsl(197deg 66% 59%) 0%,
+  hsl(201deg 71% 64%) 11%,
+  hsl(207deg 74% 69%) 22%,
+  hsl(213deg 77% 74%) 33%,
+  hsl(221deg 79% 79%) 44%,
+  hsl(232deg 78% 82%) 56%,
+  hsl(248deg 75% 84%) 67%,
+  hsl(265deg 71% 84%) 78%,
+  hsl(283deg 65% 84%) 89%,
+  hsl(303deg 62% 84%) 100%
+)`,
+      },
+      colors: {
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
+        },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))',
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))',
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))',
+        },
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))',
+        },
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))',
+        },
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
+    },
+  },
+  plugins: [],
+};
